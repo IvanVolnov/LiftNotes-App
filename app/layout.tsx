3b905254ab2738d6ce43e8a0b9531d6aefa6f59d
IvanVolnov <ivanvolnov92@gmail.com>
2024-05-31 21:56:52 +0300

    added color theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import ThemeRegistry from './theme/ThemeRegistry';
+import { CssBaseline } from '@mui/material';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,7 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <ThemeRegistry>
+        <CssBaseline />
+        <body className={inter.className}>{children}</body>
+      </ThemeRegistry>
     </html>
   );
 }
---
+++ app/theme/theme.ts
@@ -0,0 +1,30 @@
+import { Roboto } from 'next/font/google';
+import { createTheme } from '@mui/material/styles';
+import { red } from '@mui/material/colors';
+
+export const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+  fallback: ['Helvetica', 'Arial', 'sans-serif'],
+});
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#28d291',
+    },
+    secondary: {
+      main: '#28196B',
+    },
+    error: {
+      main: red.A400,
+    },
+  },
+  typography: {
+    fontFamily: roboto.style.fontFamily,
+  },
+});
+
+export default theme;
