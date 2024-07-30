e3f6fe98ea34e8550b0689ffe8f3ec95d52fc309
IvanVolnov <ivanvolnov92@gmail.com>
2024-07-30 20:57:22 +0300

    added login route to backend

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,8 +1,15 @@
 import express, { Request, Response } from 'express';
 import { sql } from '@vercel/postgres';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const users = express.Router();
+
+interface User {
+  login?: string;
+  email: string;
+  password: string;
+}
 
 users.get('/', async (req: Request, res: Response) => {
   try {
@@ -13,17 +20,42 @@ users.get('/', async (req: Request, res: Response) => {
   }
 });
 
-users.post('/', async (req: Request, res: Response) => {
+users.post('/register', async (req: Request, res: Response) => {
   try {
-    const userLogin: string = req.body.userLogin;
-    const userEmail: string = req.body.userEmail;
-    const userPassword = await bcrypt.hash(req.body.userPassword, 10);
-    if (!userLogin || !userEmail || !userPassword)
+    const { login, email, password }: User = req.body;
+    const hashedPassword: string = await bcrypt.hash(password, 10);
+
+    if (!login || !email || !password)
       throw new Error('login, email or password missing');
     const newUser = await sql`INSERT INTO users (login, email, password) 
 VALUES 
-(${userLogin}, ${userEmail}, ${userPassword}) RETURNING *;`;
+(${login}, ${email}, ${hashedPassword}) RETURNING *;`;
     return res.status(200).json({ users: newUser.rows[0] });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+users.post('/login', async (req: Request, res: Response) => {
+  try {
+    const { email, password }: User = req.body;
+
+    const users = await sql`SELECT * FROM users WHERE email = ${email};`;
+
+    if (users.rows.length === 0) {
+      return res.status(401).json('invalid email or password');
+    }
+
+    const validPassword = await bcrypt.compare(
+      password,
+      users.rows[0].password
+    );
+
+    if (!validPassword) {
+      return res.status(401).json('invalid email or password');
+    }
+
+    return res.status(200).json('Success');
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
