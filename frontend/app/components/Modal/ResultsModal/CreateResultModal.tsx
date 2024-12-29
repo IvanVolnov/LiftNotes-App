469d241b676e896154a5a2cef3d1a27e27044801
IvanVolnov <ivanvolnov92@gmail.com>
2024-12-29 19:50:22 +0300

    Add sets fields to results creation modal

--- frontend/app/components/Modal/ResultsModal/CreateResultModal.tsx
+++ frontend/app/components/Modal/ResultsModal/CreateResultModal.tsx
@@ -9,7 +9,7 @@ import {
 } from '@mui/material';
 import { DatePicker } from '@mui/x-date-pickers';
 import dayjs from 'dayjs';
-import { useState } from 'react';
+import { ChangeEvent, useState } from 'react';
 
 interface CustomProps {}
 
@@ -24,17 +24,48 @@ export default function CreateResultModal({}: CustomProps) {
     weightAmount: 0,
     weightUnit: '',
     totalSets: 0,
-    setAmount: 0,
+    setAmount: 1,
   };
 
   const [sets, setSets] = useState<ResultSet[]>([emptyResultSet]);
 
   function handleAddSet() {
+    console.log(sets);
     setSets((prev) => [...prev, emptyResultSet]);
   }
 
   function handleRemoveSet() {
     setSets((prev) => prev.slice(0, -1));
+  }
+
+  function handleChangeSet(
+    i: number,
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    key: keyof ResultSet
+  ) {
+    setSets((prev) => {
+      const updatedValue = parseValue(key, e.target.value);
+
+      return prev.toSpliced(i, 1, {
+        ...prev[i],
+        [key]: updatedValue,
+      });
+    });
+  }
+
+  function parseValue(key: keyof ResultSet, val: string) {
+    const numericFields: (keyof ResultSet)[] = [
+      'setNumber',
+      'reps',
+      'weightAmount',
+      'totalSets',
+      'setAmount',
+    ];
+
+    if (numericFields.includes(key)) {
+      return Number(val);
+    }
+    return val;
   }
 
   return (
@@ -46,68 +77,70 @@ export default function CreateResultModal({}: CustomProps) {
           name='resultDate'
           autoFocus={true}
           defaultValue={defaultDate}
+          sx={{ marginTop: '1rem' }}
         />
         <Typography variant='subtitle1' mt={3} mb={2}>
           Training sets
         </Typography>
         {sets.map((el, i) => {
-          const dataIsRequired = sets[i].href !== '' && sets[i].label === '';
+          const invalidData = false;
           return (
-            <Stack key={i} direction='row' spacing={1} mb={2}>
+            <Stack
+              key={i}
+              direction='row'
+              spacing={1}
+              mb={2}
+              sx={{ flexWrap: 'wrap' }}
+            >
               <TextField
-                error={dataIsRequired}
-                required={dataIsRequired}
+                sx={{ width: '4rem' }}
+                error={invalidData}
+                required={invalidData}
                 id={`sets-${i}`}
                 name='reps'
-                label={dataIsRequired ? 'reps is required' : 'Reps'}
+                label={invalidData ? 'invalid data' : 'Sets'}
                 type='number'
                 variant='standard'
                 margin='dense'
-                defaultValue={el.label}
-                onChange={(e) =>
-                  setSets((prev) =>
-                    prev.toSpliced(i, 1, {
-                      href: prev[i].href,
-                      label: e.target.value,
-                    })
-                  )
-                }
+                defaultValue={el.setAmount}
+                onChange={(e) => handleChangeSet(i, e, 'setAmount')}
               />
               <TextField
-                error={dataIsRequired}
-                required={dataIsRequired}
+                sx={{ width: '4rem' }}
+                error={invalidData}
+                required={invalidData}
                 id={`reps-${i}`}
                 name='reps'
-                label={dataIsRequired ? 'reps is required' : 'Reps'}
+                label={invalidData ? 'invalid data' : 'Reps'}
                 type='number'
                 variant='standard'
                 margin='dense'
-                defaultValue={el.label}
-                onChange={(e) =>
-                  setSets((prev) =>
-                    prev.toSpliced(i, 1, {
-                      href: prev[i].href,
-                      label: e.target.value,
-                    })
-                  )
-                }
+                defaultValue={el.reps}
+                onChange={(e) => handleChangeSet(i, e, 'reps')}
               />
               <TextField
-                id={`href-${i}`}
-                name='href'
-                label='Link'
-                type='text'
-                margin='dense'
+                sx={{ width: '4rem' }}
+                error={invalidData}
+                required={invalidData}
+                id={`weightAmount-${i}`}
+                name='weightAmount'
+                label={invalidData ? 'invalid data' : 'Weight'}
+                type='number'
                 variant='standard'
-                defaultValue={el.href}
-                onChange={(e) =>
-                  setSets((prev) =>
-                    prev.toSpliced(i, 1, {
-                      label: prev[i].label,
-                      href: e.target.value,
-                    })
-                  )
-                }
+                margin='dense'
+                defaultValue={el.weightAmount}
+                onChange={(e) => handleChangeSet(i, e, 'weightAmount')}
+              />
+              <TextField
+                error={invalidData}
+                required={invalidData}
+                id={`weightUnit-${i}`}
+                name='weightUnit'
+                label={invalidData ? 'invalid data' : 'Unit'}
+                variant='standard'
+                margin='dense'
+                defaultValue={el.weightUnit}
+                onChange={(e) => handleChangeSet(i, e, 'weightUnit')}
               />
             </Stack>
           );
